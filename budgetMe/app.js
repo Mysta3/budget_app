09ac1e057509369251b27f81a25cadd1e1606b32
Myshawne <Myshawnestallings@gmail.com>
2019-10-17 15:50:19 -0700

    crearte newItem to budget controller

--- budgetMe/app.js
+++ budgetMe/app.js
@@ -23,15 +23,40 @@ var budgetController = (function(){
             exp: 0,
             inc: 0
         }
+
     };
     
-    // return {
-    //     addItem: function(type, des, val){
-    //         var newItem;
+    return {
+        addItem: function(type, des, val){
+            var newItem, ID;
+             
+            //Create new ID
+            if(data.allItems[type].length > 0){
+                ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
+            }else {
+                ID = 0;
+            }
 
-    //         newItem new Expense(id, des, val);
-    //     }
-    // };
+            //Create new item base on inc or exp type.
+            if (type === 'exp'){
+                newItem = new Expense(ID, des, val);
+            } else if (type === 'inc'){
+                newItem = new Income(ID, des, val);
+            }
+            
+            //Push item into our data structures
+            data.allItems[type].push(newItem);
+
+            //return the new element
+            return newItem; 
+            
+        },
+
+        testing: function(){
+            console.log(data);
+        }
+    };
+
 })();
 
 
@@ -82,14 +107,14 @@ var controller = (function(budgetCtrl, UICtrl){
 
 
     var ctrlAdditem = function(){
-
+        var input, newItem;
         //get input values
-        var input = UIControler.getInput();
-        console.log(input);
-
+        input = UIControler.getInput();
 
 
         //add item to budget controller
+        newItem = budgetCtrl.addItem(input.type, input.description, input.value);
+
         //Add new item to user Interface
         // Calc budget 
         //display the budget on UI
